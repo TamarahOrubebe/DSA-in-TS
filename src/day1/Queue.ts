876270e70df91de3bcabcf18ebcc064c0200c132
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-03-29 07:09:37 +0100

    Add create queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -14,20 +14,32 @@ export default class Queue<T> {
         this.head = this.tail = undefined;
     }
 
-        enqueue(item: T): void {
+    enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
 
+        this.tail.next = node;
+        this.tail = node;
     }
     deque(): T | undefined {
         if (!this.head) {
             return undefined;
         }
-        
+
         this.length--;
         const head = this.head;
         this.head = this.head.next;
 
         // free in other languages that do not have garbage collection
-        head.next = undefined;
+        if (this.length === 0) {
+            head.next = undefined;
+            this.tail = undefined
+        }
+       
 
         return head.value;
 
