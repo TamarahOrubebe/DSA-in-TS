7bb52baf40373422c2810ec05cf7a0447f2f57b0
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-04-10 15:25:20 +0100

    Added code for DFSGraphList

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -2,9 +2,8 @@ export default function bfs(graph: WeightedAdjacencyMatrix, source: number, need
     const prev = new Array(graph.length).fill(-1);
     const seen = new Array(graph.length).fill(false);
 
-    const q:  number[]= [source];
+    const q: number[] = [source];
     seen[source] = true;
-
 
     while (q.length) {
         const curr = q.shift() as number;
@@ -13,36 +12,34 @@ export default function bfs(graph: WeightedAdjacencyMatrix, source: number, need
             break;
         }
 
-        const connections = graph[curr];
-        // loop through the connections and 
-        for (let i = 0; i < connections.length; i++) {
-            // continue if there is no edge
-            if (connections[i] === 0) {
+        const c: number[] = graph[curr];
+        for (let i = 0; i < c.length; i++) {
+            if (c[i] === 0) {
                 continue;
             }
 
             if (seen[i] === true) {
                 continue;
             }
-
+            
             seen[i] = true;
             prev[i] = curr;
             q.push(i);
+ 
         }
-
     }
 
-    // build it backwards to -1 to keep track of the path.
+    // go backwards to -1 to create path;
     let curr = needle;
     const path: number[] = [];
     while (prev[curr] !== -1) {
-        path.push(curr)
+        path.push(curr);
         curr = prev[curr];
     }
 
     if (path.length) {
-        return [source].concat(path.reverse());
+        return [source].concat(path.reverse())
     }
-    
-    return null;
+
+    return null
 }
