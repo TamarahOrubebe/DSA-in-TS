41d524e67a76a6e0bb1de96f36d4eba634bb5ff3
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-04-10 14:33:43 +0100

    Add code for binarySearch on a Graph

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,48 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const prev = new Array(graph.length).fill(-1);
+    const seen = new Array(graph.length).fill(false);
 
+    const q:  number[]= [source];
+    seen[source] = true;
+
+
+    while (q.length) {
+        const curr = q.shift() as number;
+
+        if (curr === needle) {
+            break;
+        }
+
+        const connections = graph[curr];
+        // loop through the connections and 
+        for (let i = 0; i < connections.length; i++) {
+            // continue if there is no edge
+            if (connections[i] === 0) {
+                continue;
+            }
+
+            if (seen[i] === true) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i);
+        }
+
+    }
+
+    // build it backwards to -1 to keep track of the path.
+    let curr = needle;
+    const path: number[] = [];
+    while (prev[curr] !== -1) {
+        path.push(curr)
+        curr = prev[curr];
+    }
+
+    if (path.length) {
+        return [source].concat(path.reverse());
+    }
+    
+    return null;
 }
